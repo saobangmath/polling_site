ff3b5b7139603d40103538eaedf9ceb4876ff134
saobangmath <anhtai2672000@gmail.com>
2019-09-02 18:10:06 +0800

    add views template for the webpage

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+
+def results(request,question_id):
+    response = "You are looking for the result of the question %s."
+    return HttpResponse(response % question_id)
+
+def detail(request,question_id):
+    response = ("Question %s." % question_id)
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Voting on question %s." % question_id)
+
+def add(request,question_id):
+    return HttpResponse("Delete on question %s." % question_id)
+
